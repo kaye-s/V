1b70b213a9cc77001a47f20817768d182bc91c8d
zhangtingen <else8072@gmail.com>
2026-05-07 17:01:44 -0500

    versionone

--- api/services/user_services.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-
-def create_user(email, password):
-    user = User.objects.create_user(
-        username=email,
-        email=email,
-        password=password
-    )
-    return user
